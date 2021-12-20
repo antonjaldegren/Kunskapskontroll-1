28c92f94bdc3c47af553c92f6663ca76552f8e70
Anton Jaldegren <anton.jaldegren@gmail.com>
2021-12-20 11:44:23 +0100

    All html pages updated with new header css and js + jQuery script tags

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -1,10 +1,6 @@
-function animateHamburgerMenu() {
+$(".hamburger-menu").on("click", () => {
+	$(".slide-in-menu").toggleClass("menu-open");
 	$(".top-bar").toggleClass("tilt-down");
 	$(".middle-bar").toggleClass("hide");
 	$(".bottom-bar").toggleClass("tilt-up");
-}
-
-$(".hamburger-menu").on("click", () => {
-	$(".slide-in-menu").toggleClass("menu-open");
-	animateHamburgerMenu();
 });
